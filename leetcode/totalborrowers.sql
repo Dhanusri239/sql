610557300a96e494151d7c5101288abbf2654040
Dhanusri239 <sdhanu946@gmail.com>
2026-02-21 13:10:03 +0530

    leetcode 3570

---
+++ leetcode/totalborrowers.sql
@@ -0,0 +1,20 @@
+
+SELECT
+l.book_id,
+l.title,
+l.author,
+l.genre,
+l.publication_year,
+COUNT(b.record_id) AS current_borrowers
+FROM library_books l
+LEFT JOIN borrowing_records b
+ON l.book_id=b.book_id
+WHERE b.return_date IS NULL
+GROUP BY b.book_id,
+l.title,
+l.author,
+l.genre,
+l.publication_year,
+l.total_copies
+HAVING COUNT(b.record_id) = l.total_copies
+ORDER BY current_borrowers DESC, l.title ASC;
